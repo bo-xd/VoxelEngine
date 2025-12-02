7835d655f4b368cf9d3ee33517ff904e816f0a9b
Bo <44844728+bo-xd@users.noreply.github.com>
2025-12-02 08:53:13 +0100

    improved Camera

--- Engine/Camera.c
+++ Engine/Camera.c
@@ -1,4 +1,5 @@
 #include "Camera.h"
+#include "utils/MathUtil.h"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_keyboard.h>
 #include <GL/gl.h>
@@ -10,19 +11,54 @@ void InitCamera(camera* cam, vec3 pos) {
     cam->pitch = 0.0f;
 }
 
-void ProcessInput(camera* cam, float deltaTime) {
-    const float speed = 3.0f * deltaTime;
-    const bool* state = SDL_GetKeyboardState(NULL);
-
-    if (state[SDL_SCANCODE_W]) cam->pos.z -= speed;
-    if (state[SDL_SCANCODE_S]) cam->pos.z += speed;
-    if (state[SDL_SCANCODE_A]) cam->pos.x -= speed;
-    if (state[SDL_SCANCODE_D]) cam->pos.x += speed;
-    if (state[SDL_SCANCODE_Q]) cam->pos.y -= speed;
-    if (state[SDL_SCANCODE_E]) cam->pos.y += speed;
+vec3 CameraFront(camera* cam) {
+    vec3 front;
+    front.x = cosf(cam->yaw * M_PI / 180.0f) * cosf(cam->pitch * M_PI / 180.0f);
+    front.y = sinf(cam->pitch * M_PI / 180.0f);
+    front.z = sinf(cam->yaw * M_PI / 180.0f) * cosf(cam->pitch * M_PI / 180.0f);
+    return Vec3Normalize(front);
 }
 
-// 🖱️ NEW: mouse look
+vec3 CameraRight(camera* cam) {
+    vec3 front = CameraFront(cam);
+    vec3 up = { 0.0f, 1.0f, 0.0f };
+    return Vec3Normalize(Vec3Cross(front, up));
+}
+
+void ProcessInput(camera* cam, float deltaTime) {
+    const float speed = 6.0f * deltaTime;
+    const bool* state = SDL_GetKeyboardState(NULL);
+
+    vec3 front = CameraFront(cam);
+    vec3 right = CameraRight(cam);
+
+    if (state[SDL_SCANCODE_W]) {
+        cam->pos.x += front.x * speed;
+        cam->pos.y += front.y * speed;
+        cam->pos.z += front.z * speed;
+    }
+    if (state[SDL_SCANCODE_S]) {
+        cam->pos.x -= front.x * speed;
+        cam->pos.y -= front.y * speed;
+        cam->pos.z -= front.z * speed;
+    }
+    if (state[SDL_SCANCODE_A]) {
+        cam->pos.x -= right.x * speed;
+        cam->pos.y -= right.y * speed;
+        cam->pos.z -= right.z * speed;
+    }
+    if (state[SDL_SCANCODE_D]) {
+        cam->pos.x += right.x * speed;
+        cam->pos.y += right.y * speed;
+        cam->pos.z += right.z * speed;
+    }
+
+    // Up/down
+    if (state[SDL_SCANCODE_E]) cam->pos.y += speed;
+    if (state[SDL_SCANCODE_Q]) cam->pos.y -= speed;
+}
+
+
 void ProcessMouseMovement(camera* cam, float xrel, float yrel) {
     const float sensitivity = 0.1f;
 
